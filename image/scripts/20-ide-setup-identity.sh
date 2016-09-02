edee6750ab0933451f4fb3fead4e6486d16c9415
Ewa Czechowska <ewa@ai-traders.com>
2016-09-02 08:42:37 +0000

    #9730 ide image scripts have variables in 1 place; proper permissions of 01-start-docker.sh

--- image/scripts/20-ide-setup-identity.sh
+++ image/scripts/20-ide-setup-identity.sh
@@ -5,11 +5,7 @@
 # Fails if any required secret or configuration file is missing.
 ###########################################################################
 
-# This is the directory we expect to be mounted as docker volume.
-# From that directory we mapt configuration and secrets files.
-ide_identity="/ide/identity"
-ide_home="/home/ide"
-ide_work="/ide/work"
+source /etc/ide.d/variables.sh
 
 # obligatory file; in order to ensure that after bash login, the ide user
 # is in /ide/work. Not obligatory but shortens end user's commands.
@@ -27,9 +23,3 @@ fi
 if [ -f "${ide_identity}/.gitconfig" ]; then
   cp "${ide_identity}/.gitconfig" "${ide_home}"
 fi
-
-# Absolute path to this script
-script_path=$(readlink -f "$BASH_SOURCE")
-# Absolute path to a directory this script is in
-script_dir=$(dirname "${script_path}")
-source ${script_dir}/variables.sh
--- image/scripts/variables.sh
+++ image/scripts/variables.sh
@@ -5,5 +5,13 @@
 # is copied into ideide docker image.
 ###########################################################################
 
+# This is the directory we expect to be mounted as docker volume.
+# From that directory we know uid and gid.
+export ide_work="/ide/work"
+export ide_home="/home/ide"
+export ide_identity="/ide/identity"
+export owner_username="ide"
+export owner_groupname="ide"
+
 export this_image_name="ideide"
 export this_image_tag="1.0.0"
